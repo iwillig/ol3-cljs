fe9d5074395b6eb8b5bcc6c2533d8396f5130a10
Ivan Willig <iwillig@gmail.com>
2014-08-14 08:22:44 -0400

    Initial commit

---
+++ src/ol3_cljs/core.cljs
@@ -0,0 +1,10 @@
+(ns ol3-cljs.core
+  (:import
+   [ol Map]
+   [ol.source MapQuest]))
+
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/ol3_cljs/core_test.clj
@@ -0,0 +1,7 @@
+(ns ol3-cljs.core-test
+  (:require [clojure.test :refer :all]
+            [ol3-cljs.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
